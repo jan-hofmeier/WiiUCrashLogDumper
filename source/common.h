c2c1e7db6a23a62c71fdd1262ef180456f8c0337
Jan Hofmeier <jan.hofmeier@gmail.com>
2025-01-26 20:24:29 +0100

    change mountpoint

--- source/common.h
+++ source/common.h
@@ -2,7 +2,7 @@
 #include "version.h"
 
 #define CRASH_LOG_DESTINATION_PATH "fs:/vol/external01/wiiu/crash_logs"
-#define SLC_STORAGE_PATH           "storage_slc_wiiuident"
+#define SLC_STORAGE_PATH           "storage_slc_logdumper"
 #define CRASH_LOG_SOURCE_PATH      SLC_STORAGE_PATH ":/sys/logs"
 
 #define APP_VERSION                "v0.1"
